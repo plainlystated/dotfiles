3ab38be1f51fb492f38305d023edadd0d51042b3
Patrick Schless <patrick.schless@gmail.com>
2017-06-01 11:07:09 -0500

    comment out things I dont think I need anymore

--- linker.rb
+++ linker.rb
@@ -1,5 +1,7 @@
 working_dir = File.expand_path(File.dirname(__FILE__))
 
+  #oh-my-zsh
+  #zshrc
 %w[
   bash_profile
   bashrc
@@ -8,8 +10,6 @@ working_dir = File.expand_path(File.dirname(__FILE__))
   tmux.conf
   vim
   vimrc
-  oh-my-zsh
-  zshrc
 ].each do |file|
   cmd = "ln -s #{File.join(working_dir, file)} ~/.#{file}"
   puts cmd
@@ -20,6 +20,6 @@ cmd = "ln -s #{File.join(working_dir, 'ssh_config')} ~/.ssh/config"
 puts cmd
 `#{cmd}`
 
-cmd = "ln -s #{File.join(working_dir, 'i3-config')} ~/.i3/config"
-puts cmd
-`#{cmd}`
+#cmd = "ln -s #{File.join(working_dir, 'i3-config')} ~/.i3/config"
+#puts cmd
+#`#{cmd}`
